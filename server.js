40716fb8e1952ad2945e1e8899c3782b3bb2db49
karlr <karlrobert12345@gmail.com>
2020-04-19 22:23:30 +0300

    authenticate ja bootstrap

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 require('dotenv').config()
 const express = require('express')
 const bodyparser = require('body-parser')
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const easySession = require('easy-session');
 const knex = require('knex')({
     client: 'mysql2',
     connection: {
@@ -12,15 +15,24 @@ const knex = require('knex')({
   }
 })
 const app = express();
-const port = 2800;
+const port = 3000;
 const bcrypt = require('bcrypt');
 
+
+app.use(cookieParser());
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true
+}))
+app.use(easySession.main(session));
 
 
 app.use(bodyparser.json())
 app.use('/', express.static('web'));
 
 app.get('/api/record', function(req, res) {
+  //req.session.user
   knex('diary').select().then(function (result) {
     res.send(result)
 })
@@ -33,7 +45,41 @@ app.post('/api/record', function(req, res) {
 
   })
 })
+app.post('/api/login', function (req, res) {
+  const {loginEmail, loginPassword} = req.body
+  if (!loginEmail || !loginPassword ){
+    return res.status(400).send('Email or Password is invalid')
+  }
+  knex('user')
+      .select('id', 'email', 'password', 'firstname', 'lastname')
+      .where({
+        email: loginEmail,
+      })
+      .first()
+      .then( function (result) {
+        const {email, password} = result
+        if (!email || !password){
+          return res.status(403).send('Email or password does not fit');
+        }
+        const doesPasswordMatch = bcrypt.compareSync(loginPassword, password)
+        if (!doesPasswordMatch){
+          return res.status(403).send('Email or password does not fit');
+        }
+        const user = {
+          userId: result.id,
+          firstName: result.firstname,
+          lastName: result.lastname,
+          email: result.email
+        }
+        req.session.login({user})
+            .then(() => {
+              //here we have a logged in session
+              console.log(req.session.userId); // Will print 10;
+              res.send('Authentigated')
+            });
 
+  })
+})
 app.post('/api/registration', function (req, res) {
   const data = req.body
   data.password = bcrypt.hashSync(data.password, 10);
